141d723105270e06d6fa390b8d9c36096ee39f8e
Afsal897 <afsalsalim897@gmail.com>
2024-12-09 21:03:59 +0530

    Add files via upload

---
+++ sample/script.js
@@ -0,0 +1,11 @@
+// Smooth scroll to sections when clicking links (optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
